4b1ab775bcff618d67d8d5cacc1e555a9f053263
sund <sund@saejong>
2023-04-12 13:28:30 +0800

    TcpServer可以与多个client并发连接

--- TcpServer.cs
+++ TcpServer.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media.Imaging;
 using System.Windows.Threading;
@@ -15,14 +15,14 @@ namespace Spot;
 public class TcpServer
 {
     private TcpListener _listener;
-    private Thread _thread;
+    private Task _thread;
     private Dispatcher _dispatcher;
     private bool _isRunning;
 
     public TcpServer(IPAddress ipAddress, int port)
     {
         _listener = new TcpListener(ipAddress, port);
-        _thread = new Thread(ListenForClients);
+        _thread = new Task(ListenForClients, TaskCreationOptions.LongRunning);
         _dispatcher = Application.Current.Dispatcher;
         _isRunning = false;
     }
@@ -55,71 +55,78 @@ public class TcpServer
             try
             {
                 var client = _listener.AcceptTcpClient();
-
-                var stream = client.GetStream();
-                int i;
-
-                MainWindow? mainWindow = default;
-                List<SightWindow>? sightWindows = default;
-                _dispatcher.Invoke(() =>
+                Task.Run(() =>
                 {
-                    mainWindow = Application.Current.Windows.Cast<Window>()
-                        .FirstOrDefault(w => w is MainWindow) as MainWindow;
-                    sightWindows = Application.Current.Windows.Cast<Window>()
-                        .OrderBy(window => window.Title)
-                        .Where(window => window is SightWindow)
-                        .Cast<SightWindow>()
-                        .ToList();
+                    try
+                    {
+                        var stream = client.GetStream();
+                        int i;
+
+                        MainWindow? mainWindow = default;
+                        List<SightWindow>? sightWindows = default;
+                        _dispatcher.Invoke(() =>
+                        {
+                            mainWindow = Application.Current.Windows.Cast<Window>()
+                                .FirstOrDefault(w => w is MainWindow) as MainWindow;
+                            sightWindows = Application.Current.Windows.Cast<Window>()
+                                .OrderBy(window => window.Title)
+                                .Where(window => window is SightWindow)
+                                .Cast<SightWindow>()
+                                .ToList();
+                        });
+                        if (mainWindow is null || sightWindows is null)
+                        {
+                            stream.Write(fail, 0, fail.Length);
+                            client.Close();
+                            return;
+                        }
+
+                        while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                        {
+                            var data = Encoding.UTF8.GetString(bytes, 0, i);
+
+                            var datas = data.Split(",", 2, StringSplitOptions.TrimEntries);
+                            if (datas.Length != 2 || !int.TryParse(datas[0], out var index) || datas[1] == string.Empty)
+                            {
+                                stream.Write(invalidRequest, 0, invalidRequest.Length);
+                                break;
+                            }
+
+                            var imagePath = datas[1];
+                            if (index > sightWindows.Count)
+                            {
+                                stream.Write(outOfRange, 0, outOfRange.Length);
+                                break;
+                            }
+
+                            index--;
+
+                            _dispatcher.Invoke(() =>
+                            {
+                                var sightWindow = sightWindows[index];
+                                sightWindow.Title = $"Spot #{index + 1}";
+                                sightWindow.Title += $" -> Image: \"{imagePath}\"";
+                                sightWindow.SightImage.Source = default;
+
+                                if (File.Exists(imagePath))
+                                {
+                                    sightWindow.SightImage.Source = new BitmapImage(new Uri(imagePath));
+                                    sightWindow.Title += " -> OK";
+                                    stream.Write(ok, 0, ok.Length);
+                                }
+                                else
+                                {
+                                    sightWindow.Title += " -> Error: image not found";
+                                    stream.Write(notFound, 0, notFound.Length);
+                                }
+                            });
+                        }
+                    }
+                    finally
+                    {
+                        client.Close();
+                    }
                 });
-                if (mainWindow is null || sightWindows is null)
-                {
-                    stream.Write(fail, 0, fail.Length);
-                    client.Close();
-                    continue;
-                }
-
-                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
-                {
-                    var data = Encoding.UTF8.GetString(bytes, 0, i);
-
-                    var datas = data.Split(",", 2, StringSplitOptions.TrimEntries);
-                    if (datas.Length != 2 || !int.TryParse(datas[0], out var index) || datas[1] == string.Empty)
-                    {
-                        stream.Write(invalidRequest, 0, invalidRequest.Length);
-                        break;
-                    }
-
-                    var imagePath = datas[1];
-                    if (index > sightWindows.Count)
-                    {
-                        stream.Write(outOfRange, 0, outOfRange.Length);
-                        break;
-                    }
-
-                    index--;
-
-                    _dispatcher.Invoke(() =>
-                    {
-                        var sightWindow = sightWindows[index];
-                        sightWindow.Title = $"Spot #{index + 1}";
-                        sightWindow.Title += $" -> Image: \"{imagePath}\"";
-                        sightWindow.SightImage.Source = default;
-
-                        if (File.Exists(imagePath))
-                        {
-                            sightWindow.SightImage.Source = new BitmapImage(new Uri(imagePath));
-                            sightWindow.Title += " -> OK";
-                            stream.Write(ok, 0, ok.Length);
-                        }
-                        else
-                        {
-                            sightWindow.Title += " -> Error: image not found";
-                            stream.Write(notFound, 0, notFound.Length);
-                        }
-                    });
-                }
-
-                client.Close();
             }
             catch (SocketException)
             {
